345805cc552fb08f21864f58367bdf27c99634d2
amineromdhane77 <amineromdhane77@gmail.com>
2023-09-09 19:35:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function slider(amine){
+    document.querySelector('.pepci').src=amine;
+}
+function ChangeColor(color){
+    const sec=document.querySelector('.sec');
+    sec.style.background=color
+}
